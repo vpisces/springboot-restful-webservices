9e8dc4887c9f862d3cfec6561350b9974697ffb0
vpisces <106975635+vpisces@users.noreply.github.com>
2023-05-16 22:07:08 +0530

    first-commit

---
+++ src/main/java/net/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
@@ -0,0 +1,45 @@
+package net.javaguides.springboot;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot REST API Documentation",
+				description = "Spring Boot REST API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Faiyaz Raza",
+						email = "faiyazraza@mail.com",
+						url = "https://www.google.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.google.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot User Management Documentation",
+				url = "https://www.google.com"
+		)
+)
+public class SpringbootRestfulWebservicesApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulWebservicesApplication.class, args);
+	}
+
+}
